9b18168091145f0cd4a2d85819c08726d34e1127
vagabondms <hiseokseok@gmail.com>
2021-07-04 15:51:52 +0900

    add hover condition on requestCard

--- src/components/RequestCard/styles.js
+++ src/components/RequestCard/styles.js
@@ -5,9 +5,13 @@ export const CardWrapper = styled.div`
 	@media (max-width: 560px) {
 		grid-column: auto / span 3;
 	}
+	box-sizing: border-box;
 	height: 356px;
 	border: 1px solid #e5e5e5;
 	display: flex;
 	flex-direction: column;
 	padding: 24px 16px;
+	&:hover {
+		box-shadow: 0.5px 0.5px 2px 2px #2196f3;
+	}
 `;
